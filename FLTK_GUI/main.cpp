359c7558983f25e2c2375b2576988d80709eb191
diego <diego@DIEGO-LAPTOP>
2024-06-18 17:51:41 -0600

    Main WindowsChanges: Added new text Labels, increases windows size.
    Evaluator.h and cpp: added new Vector cllaed postfix hist, added a new line of code, in function infixToPostFi line 141

--- FLTK_GUI/main.cpp
+++ FLTK_GUI/main.cpp
@@ -45,7 +45,8 @@ void button_callback(Fl_Widget* widget, void* data) {
 
 
 
-void eval_expression_callback(Fl_Widget* widget, void * data) {
+void eval_expression_callback(Fl_Widget* widget, void * data) {	
+
 	Linker* my_data = static_cast<Linker*>(data);
 	// Access the data
 	Fl_Multiline_Input* input = my_data->expression;
@@ -57,13 +58,22 @@ void eval_expression_callback(Fl_Widget* widget, void * data) {
 // 
 //	evaluator->loadUserConstants(table->g;et_data());
 	std::string value = input->value();
+	if (value == "") {
+		return;
+	}
 	evaluator->inputExpression(value.c_str(), false);
-	std::string prev = (my_data->history->value());
+	std::string prev_hist= (my_data->history->value());
 
-	my_data->history->value((prev + "\n" + value).c_str());
 
+	my_data->history->value((prev_hist + "\n" + value).c_str());
 
 	std::string line="";
+	for (std::string it:evaluator->posfixHistory) {
+		std::cout << line;
+		line = line + it;
+		
+	}
+	my_data->postfix->value(line.c_str());
 	//We could had  to clean teh output
 	std::string output=evaluator->displayHistory();
 	display_hist_output(my_data->output, output);
@@ -80,6 +90,9 @@ void assign_expression_callback(Fl_Widget* widget, void* data) {
 	Fl_Multiline_Output* variable_output = my_data->output;
 
 	std::string value = input->value();
+	if (value == "") {
+		return;
+	}
 	evaluator->inputExpression(value.c_str(), true);
 
 	std::string prev = (my_data->history->value());
@@ -106,15 +119,12 @@ void show_button_callback(Fl_Widget* widget, void* data) {
 }
 void grid(int x, Fl_Multiline_Input* in,Evaluator *evaluator){
 	//pi, e, g, c
-	Fl_Multiline_Output * out=new Fl_Multiline_Output(x, 20, 170, 20 * 4);
+	Fl_Multiline_Output * out=new Fl_Multiline_Output(x, 20, 200, 20 * 4);
 	std::string line;
 	for (auto it = evaluator->constants.begin(); it != evaluator->constants.end(); it++) {
 		line = line + (it->first + "=" + std::to_string(it->second) + "\n");
 		out->value(line.c_str());
 	}
-
-
-
 
 }
 
@@ -125,6 +135,15 @@ void grid(int x, Fl_Multiline_Input* in,Evaluator *evaluator){
 
 const char* get_value(Fl_Multiline_Input* in) {
 	return in->value();
+}
+
+void drawText(int x , int y, int w, int h,const char* text) {
+
+	Fl_Label* label2 = new Fl_Label();
+	Fl_Box* box2 = new Fl_Box(x,y,w,h, text);
+	box2->labelsize(20);
+	box2->labelfont(FL_SCREEN);
+
 }
 
 
@@ -138,37 +157,43 @@ int main(int argc, char** argv) {
 
 
 
-	Fl_Window* window = new Fl_Window(600, 400);
+	Fl_Window* window = new Fl_Window(800,500);
 
-	int x = 600;
-	int y = 400;
+	int x = 800;
+	int y = 500;
+	int widths = 200;
 
-	Fl_Multiline_Input* input_expression = new Fl_Multiline_Input((x / 2) - 100, 20, 200, 50, nullptr);
-	Fl_Button* button_eval = new Fl_Button((x / 2) - 100, 20 + 60, 170, 20, "Evaluate");
-	//We link the inut with Linke Struct.
+	//Creating  Text Inputs and Otpus
+	Fl_Multiline_Output* out_put = new Fl_Multiline_Output((x / 2) - 100, (y / 2) - 150, widths, y/2 -100 +50, nullptr);
+	Fl_Multiline_Output* post_out_put = new Fl_Multiline_Output((x / 2) - 100, (y / 2)+70 ,widths, y / 2 - 100, nullptr);
+	Fl_Multiline_Output* History =new Fl_Multiline_Output(x -200,20, widths, y-40, nullptr);
+	Fl_Multiline_Output* asignation_output = new Fl_Multiline_Output(20, 140, widths, y - 120);
+	Fl_Multiline_Input* asignation_input = new Fl_Multiline_Input(20,100,widths,20,"");
+	Fl_Multiline_Input* input_expression = new Fl_Multiline_Input((x / 2) - 100, 20, widths, 50, nullptr);
+
+	drawText((x / 2) - 100, (y / 2) + 50, widths, 20, "Post Fix");
+	drawText(0, 0, x,20,"Constants\t\tEvaluator\t\tHistory");
+
+
+
+
+	//box->labeltype(FL_FREE_LABELTYPE);
+
+	Fl_Button* button_eval = new Fl_Button((x / 2) - 100, 20 + 60, widths, 20, "Evaluate");
+	Fl_Button* button_show = new Fl_Button(x - 170, y - 20, widths, 20, "Show");
+	Fl_Button* button_assing = new Fl_Button(20, 120, widths, 20, "Assign");
 
 	button_eval->type(FL_NORMAL_BUTTON);
 	button_eval->callback(eval_expression_callback, link);
-	Fl_Multiline_Output* out_put = new Fl_Multiline_Output((x / 2) - 100, (y / 2) - 100, 200, 390 - ((y / 2) - 100), nullptr);
-	Fl_Multiline_Output* History =new Fl_Multiline_Output(x -200,20, 200, y-40, nullptr);
-	grid(20, input_expression,evaluator);
-	Fl_Multiline_Input* asignation_input = new Fl_Multiline_Input(20,100,170,20,"");
-	Fl_Button* button_assing = new Fl_Button(20, 120, 170, 20, "Assign");
 	button_assing->type(FL_NORMAL_BUTTON);
 	button_assing->callback(assign_expression_callback, var_link);
-
-
-	Fl_Multiline_Output* asignation_output = new Fl_Multiline_Output(20, 140, 170, y - 120);
-
-	
-	Fl_Button* button_show = new Fl_Button(x-170,y-20, 170, 20, "Show");
-
+	grid(20, input_expression, evaluator);
 
 	link->history = History;
 	link->evaluator = evaluator;
 	link->output = out_put;
 	link->expression = input_expression;
-
+	link->postfix = post_out_put;
 	var_link->evaluator = evaluator;
 	var_link->output = asignation_output;
 	var_link->history = History;
